36ec5ad261c2c78fd6152292341847b917163077
JT <j.j.teijema@gmail.com>
2022-02-07 14:41:44 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,9 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
 
+import datetime
+year = datetime.datetime.now().year
+
 # -- Project information
 
 project = 'EU-Alternative'
-copyright = '2022, github.com/JTeijema'
+copyright = '{}, EU Alternative'.format(year)
 author = 'github.com/JTeijema'
 
 release = '0.1'
@@ -27,9 +30,42 @@ intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst']
+source_suffix = ['.rst']
+
+# The master toctree document.
+root_doc = 'index'
+
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = "sphinx_rtd_theme"
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    "globaltoc_collapse": "true"
+}
+
+html_static_path = ['_static']
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_css_files = ["css/EUtheme.css"]
